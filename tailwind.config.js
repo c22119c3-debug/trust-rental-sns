eb38951db75af2a4e9c8476af320de3618939a84
c22119c3-debug <c22119c3@shibaurafzk.com>
2026-04-22 10:05:36 +0900

    Add Tailwind CSS configuration file for dark mode support

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // or 'media' for media-query-based dark mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
